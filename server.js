98c343f783158ab4e16e8681caf6559ad96c4531
jaldhara.patel <jaldharapatel91@gmail.com>
2023-08-17 21:01:35 -0400

    added middleware, Connect to mongoDB in server.js

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 // Import the 'express' module
-const express = require('express');
+const express = require("express");
+const db = require("./config/connection");
+const routes = "./routes";
 
+//Set environment variables
 const port = process.env.PORT || 3001;
 const app = express();
+
+// Middleware to parse incoming data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+// connect to the MongoDB database, start the server and listen on the port
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
