27da1f031f142ee348597430ab7fccf38c538c16
jaldhara.patel <jaldharapatel91@gmail.com>
2023-08-17 20:59:47 -0400

    Added models files

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// Import the 'User' and 'Thought' models from the js file
+const User = require("./User");
+const Thought = require("./Thought");
+
+// Export the models as an object
+module.exports = { User, Thought };
